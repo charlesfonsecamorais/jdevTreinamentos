4d66dd130bada1e101a1d0d83f82ef934eb8d925
charlesfonsecamorais <charlesfonsecamorais@gmail.com>
2024-09-16 15:31:28 -0300

    Criação do Projeto

---
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -0,0 +1,86 @@
+package br.com.springboot.curso_jdev_treinamento.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.springboot.curso_jdev_treinamento.model.Usuario;
+import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
+
+/**
+ *
+ * A sample greetings controller to return greeting text
+ */
+@RestController
+public class GreetingsController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+    /**
+     *
+     * @param name the name to greet
+     * @return greeting text
+     */
+    
+    @GetMapping(value = "listartodos")
+    @ResponseBody
+    public ResponseEntity<List<Usuario>> listaUsuario(){
+    	List<Usuario> usuarios = usuarioRepository.findAll();
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+    }
+    
+    @PostMapping(value = "salvar")
+    @ResponseBody
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){
+    	Usuario user = usuarioRepository.save(usuario);
+    	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+    }
+        
+    @DeleteMapping(value = "apagar")
+    @ResponseBody
+    public ResponseEntity<String> apagar(@RequestParam Long idUser){
+    	usuarioRepository.deleteById(idUser);
+    	return new ResponseEntity<String>("Usuário excluído com sucesso!", HttpStatus.OK);
+    }
+    
+    @PutMapping(value = "atualizar")
+    @ResponseBody
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario){
+    	if(usuario.getId() == null) {
+    		return new ResponseEntity<String>("Id não informado para atualização!", HttpStatus.OK);
+    	}
+    	Usuario user = usuarioRepository.saveAndFlush(usuario);
+    	return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+    }
+    
+    @GetMapping(value = "buscarusuario")
+    @ResponseBody
+    public ResponseEntity<Usuario> buscarusuario(@RequestParam (name = "idUser") Long idUser){
+    	Usuario usuario = usuarioRepository.findById(idUser).get();
+    	return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+    }
+    
+    @GetMapping(value = "buscarPorNome")
+    @ResponseBody
+    public ResponseEntity<List<Usuario>> buscarPorNome(@RequestParam (name = "name") String name){
+    	List<Usuario> usuario = usuarioRepository.buscarPorNome(name.trim().toUpperCase());
+    	return new ResponseEntity<List<Usuario>>(usuario, HttpStatus.OK);
+		
+    	
+    }
+    
+}
